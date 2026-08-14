86f4f6adc069f2fbe80a72f66f99d0ddb04e38c4
2chris23 <uribec080@gmail.com>
2026-08-13 20:47:58 -0400

    Fix: add Compresion and CompresionMax middleware with aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Configuraciones de middleware
+        $middleware->alias([
+            'Compresion' => \App\Http\Middleware\Compresion::class,
+            'CompresionMax' => \App\Http\Middleware\CompresionMax::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

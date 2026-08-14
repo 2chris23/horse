9a22948a9839cd1fef5cde65c7cac04a2c9d058d
2chris23 <uribec080@gmail.com>
2026-08-13 22:52:52 -0400

    Remove scratch files

--- scratch_search.php
+++
@@ -1,7 +0,0 @@
-<?php
-foreach (glob('routes/*.php') as $file) {
-    $content = file_get_contents($file);
-    if (strpos($content, 'cliente.') !== false || strpos($content, 'state.ajax') !== false) {
-        echo "$file contains cliente. or state.ajax\n";
-    }
-}

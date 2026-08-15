3351056af686149aa8645a00ba9c3d02c5454e52
2chris23 <uribec080@gmail.com>
2026-08-14 22:15:08 -0400

    Trust Plesk proxies to fix HTTPS redirect loop

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'Compresion' => \App\Http\Middleware\Compresion::class,
             'CompresionMax' => \App\Http\Middleware\CompresionMax::class,

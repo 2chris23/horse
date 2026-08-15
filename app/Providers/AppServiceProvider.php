c54388748440fb96d1fb80dfcd828c5e918cc911
2chris23 <uribec080@gmail.com>
2026-08-14 22:33:05 -0400

    Force HTTPS scheme to resolve redirect loop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
         Schema::defaultStringLength(191);
         \Illuminate\Foundation\AliasLoader::getInstance()->alias('Funciones', \App\Http\Controllers\Functions::class);
 

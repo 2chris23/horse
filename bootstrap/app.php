ec5712a411bb96e5c7131b1e6b24ec1b3d4e5d43
2chris23 <uribec080@gmail.com>
2026-08-15 21:05:14 -0400

    Import Route facade in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
+
     ->withRouting(
         commands: __DIR__.'/../routes/console.php',
         health: '/up',

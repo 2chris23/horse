611ddbcad1fe85fd2cd4face46f8648eb011f313
2chris23 <uribec080@gmail.com>
2026-08-16 20:07:07 -0400

    Create missing models (Orden, Servicio, Codigopromo, ClientesVisita, Cliente)

---
+++ app/Models/Orden.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orden extends Model
+{
+    protected $table = 'ordenes'; // Most likely table name in Spanish
+    protected $guarded = [];
+}
